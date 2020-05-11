27fbd541775729ac45983fccd463e640ba117b41
mithunsingh2691 <mithunsingh2691@gmail.com>
2020-05-11 14:58:42 +0800

    performance and tooltip

--- src/components/map/objects/zoomObject.js
+++ src/components/map/objects/zoomObject.js
@@ -1,4 +1,5 @@
 export const zoomObjectRender = ( chartObj, am4maps, am4core ) => {
+  chartObj.zoomDuration = 100;
       // Zoom Slider
       chartObj.zoomControl = new am4maps.ZoomControl();
       chartObj.zoomControl.align = "right";
